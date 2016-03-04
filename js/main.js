1c5993149c5a005dce829735626045d9696566a8
SarahDowling <SarahandMac2@Sarahs-MacBook-Pro.local>
2016-03-04 02:51:29 -0500

    wrote story b

--- js/main.js
+++ js/main.js
@@ -14,7 +14,9 @@ if (story == 'a') {
   document.write('After snacking on ' + adjective + ' treats, the ' + colour + ' bellied ' + creature + ' ' + verbed + ' for hours.');
 }
 
-Ameilia {verbed} through the {adjective} {colour} nebula escaping the space {creature}.
+if (story == 'b') {
+  document.write('Ameilia ' + verbed + ' through the ' + adjective + ' ' + colour + ' nebula escaping the space creature.');
+}
 
 Jackson chose his {adjective}, {colour} {creature} card and {verbed} it to the table knowing he won.
 
